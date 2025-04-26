aa3d77bbb4b01d1e3cc3eb8a875218823720d24e
Serhii <146414705+SerhiiKysil@users.noreply.github.com>
2025-04-26 14:25:16 +0300

    added opportunity to select zone you need

--- src/app/components/MapView.tsx
+++ src/app/components/MapView.tsx
@@ -1,23 +1,49 @@
 'use client';
 
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, FeatureGroup } from "react-leaflet";
+import { EditControl } from "react-leaflet-draw";
 import "leaflet/dist/leaflet.css";
-import "../utils/fixLeafletIcon";
+import "leaflet-draw/dist/leaflet.draw.css";
+
+import { useRef } from "react";
 
 export default function MapView() {
+  const featureGroupRef = useRef(null);
+
+  const onCreated = (e: any) => {
+    const { layerType, layer } = e;
+    if (layerType === 'rectangle' || layerType === 'polygon') {
+      const bounds = layer.getBounds();
+      console.log('Виділена область: ', bounds);
+    }
+  };
+
   return (
-    <div className="bg-white relative min-h-screen  text-black">
+    <div className="relative min-h-screen">
       <MapContainer
-        center={[50.0020, 36.3074]} 
+        center={[49.99712258003538, 36.29473860661989]} 
         zoom={13}
-        zoomControl={false}
         style={{ height: "100vh", width: "100%" }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a>'
+          attribution='&copy; OpenStreetMap contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[50.0020, 36.3074]} />
+
+        <FeatureGroup ref={featureGroupRef}>
+          <EditControl
+            position="topright"
+            onCreated={onCreated}
+            draw={{
+              rectangle: true,
+              polygon: true,
+              polyline: false,
+              circle: false,
+              circlemarker: false,
+              marker: false,
+            }}
+          />
+        </FeatureGroup>
       </MapContainer>
     </div>
   );
