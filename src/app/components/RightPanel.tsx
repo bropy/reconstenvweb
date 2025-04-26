c37be0c2403cae2999a2aa81e937abf737a56ada
Admin <olexandr.11.f@gmail.com>
2025-04-26 18:24:23 +0300

    Add Building Count

--- src/app/components/RightPanel.tsx
+++ src/app/components/RightPanel.tsx
@@ -1,10 +1,37 @@
+import places from "../data/Places_response.json";
 
-type Props = {}
+type Props = {};
 
 function RightPanel({}: Props) {
+
   return (
-    <div className="w-64">RightPanel</div>
-  )
+    <div className="w-64">
+        <h1 className="m-2">Кількість будівель</h1>
+      {Object.entries(places.results).map(([key, value]) => (
+        <div
+          className="bg-gray-300 p-2 rounded-md w-full flex item mb-4"
+          key={key}
+        >
+          <div key={key} className="flex justify-between w-full items-center">
+            <h2>{key}</h2>
+            <div>Count: {value.count}</div>
+          </div>
+          {/* <div className="">
+            <h3>Places:</h3>
+            <ul className="list-disc list-inside">
+                {value.items.map((place: any, index: number) => (
+                <li key={index}>
+                    <div className="flex flex-col">
+                    <span>{place.name}</span>
+                    </div>
+                </li>
+                ))}
+            </ul>
+            </div> */}
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default RightPanel
+export default RightPanel;
--- src/app/components/TimeLineSelector.tsx
+++ src/app/components/TimeLineSelector.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 
-const steps = ["Будинки", "Школи лікарні", "Дороги"];
+const steps = ["Будинки", "Магазини", "Лікарні", "Школи"];
 
 export default function TimeLineSelector() {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -13,21 +13,27 @@ export default function TimeLineSelector() {
         TimeLine
       </h2>
       <div className="relative flex flex-col items-center w-full">
-        {/* Вертикальна лінія */}
-        <div className="absolute top-3 left-4 w-2 h-30 bg-gray-600"></div>
-
         {steps.map((step, index) => (
           <div
             key={index}
             className="flex items-center w-full mb-4 relative cursor-pointer ms-0"
             onClick={() => setCurrentIndex(index)}
           >
+            {index != 0 && (
+              <div className="absolute top-0 left-4 w-2 h-5 bg-gray-600" />
+            )}
+
+            {index != (steps.length - 1) && (
+              <div className="absolute top-4 left-4 w-2 h-10 bg-gray-600" />
+            )}
             {/* Крапка */}
             <div
               className={`z-10 w-6 h-6 rounded-full flex items-center justify-center text-white ${
                 currentIndex === index ? "bg-sky-600" : "bg-black"
               } ml-2`}
-            >{index+1}</div>
+            >
+              {index + 1}
+            </div>
 
             {/* Текст */}
             <div
