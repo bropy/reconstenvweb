7d78f19d2b39616fba9d6ffecedccc610e2edb85
Admin <olexandr.11.f@gmail.com>
2025-04-26 16:30:42 +0300

    Add LeftPanel + TimeLine selector

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,11 +1,15 @@
-'use client';
+"use client";
 
-import MapView from '../components/MapView';
+import MapView from "../components/MapView";
+import SidePanel from "../components/SidePanel";
 
 export default function MapPage() {
   return (
-    <div className="h-screen w-screen">
-      <MapView />
+    <div className="h-screen w-screen flex">
+      <SidePanel />
+      <div className="flex-1 h-full">
+        <MapView />
+      </div>
     </div>
   );
 }
